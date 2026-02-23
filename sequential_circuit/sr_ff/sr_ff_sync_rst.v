d12da346a6e5eea16abea37686c8ca42bab45d5e
rnaveenprakash100 <rnaveenprakash100@gmail.com>
2026-02-23 13:55:38 +0530

    Create sr_ff_sync_rst.v

---
+++ sequential_circuit/sr_ff/sr_ff_sync_rst.v
@@ -0,0 +1,18 @@
+module sr_ff(
+input clk,rst,s,r,
+output reg q
+);
+always@(posedge clk or posedge rst)
+begin
+if(rst)
+q<=0;
+else
+begin
+  case({s,r})
+     2'b00:q<=q;
+     2'b01:q<=0;
+     2'b10:q<=1;
+     2'b11:q<=1'bx;
+  endcase
+end
+endmodule
