b6102d44fa50af68cb6aa4b403e49b6caeb28598
rnaveenprakash100 <rnaveenprakash100@gmail.com>
2026-02-23 13:44:50 +0530

    Update jk_ff.v

--- sequential_circuit/jk_ff/jk_ff.v
+++ sequential_circuit/jk_ff/jk_ff.v
@@ -8,14 +8,14 @@ if(rst)
 q<=0;
 else
 begin
-if(j==0&&k==0)
-q<=q;
-else if(j==0&&k==1)
-q<=0;
-else if(j==1&&k==0)
-q<=1;
-else if(j==1&&k==1)
-q<=~q;
+   if(j==0&&k==0)
+      q<=q;
+   else if(j==0&&k==1)
+      q<=0;
+   else if(j==1&&k==0)
+       q<=1;
+  else if(j==1&&k==1)
+       q<=~q;
 end
 end
 endmodule
